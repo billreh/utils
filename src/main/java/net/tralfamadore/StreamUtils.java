b1dcd1724fa71e008f2995f05f776a737be80ae8
billreh <billreh@gmail.com>
2017-05-04 11:58:17 -0400

    updates

--- src/main/java/net/tralfamadore/StreamUtils.java
+++ src/main/java/net/tralfamadore/StreamUtils.java
@@ -1,5 +1,6 @@
 package net.tralfamadore;
 
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
@@ -11,13 +12,13 @@ import java.util.stream.Collectors;
  * Created by billreh on 4/28/17.
  */
 public class StreamUtils {
-    public static <T> List<T> filter(List<T> list, Predicate<T> predicate) {
+    public static <T> List<T> filter(Collection<T> list, Predicate<T> predicate) {
         if(list == null)
             return Collections.emptyList();
         return list.stream().filter(predicate).collect(Collectors.toList());
     }
 
-    public static <T,R> List<R> map(List<T> list, Function<T,R> function) {
+    public static <T,R> List<R> map(Collection<T> list, Function<T,R> function) {
         if(list == null)
             return Collections.emptyList();
         return list.stream().map(function).collect(Collectors.toList());
